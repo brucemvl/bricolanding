717256c779b0dd4d6cf22d2bd49daf53329c11b7
bruce <bruce.monnerville@hotmail.fr>
2026-04-14 19:06:46 +0200

    chngeme,nts index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -22,7 +22,7 @@ function Index(){
 
         <article className="client">
             <aside>
-            <h2>Vous cherchez un artisan pour réaliser des travaux chez vous?</h2>
+            <h2>Vous cherchez un bricoleur pour réaliser des travaux chez vous?</h2>
             <h3>
     Publiez votre demande et recevez des propositions rapidement.
   </h3>
